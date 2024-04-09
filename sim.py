3b68f87153289c55a13d4e4eac866b985d4435bc
visusadastra <enhupgu@DESKTOP-072BR3B>
2024-04-08 23:49:08 -0400

    update .py

--- sim.py
+++ sim.py
@@ -31,13 +31,12 @@ def collect_data(env, model, policy, target, num_trajectories=100, num_timesteps
     policy = policy_class(env)
     num_success = 0
     num_saved = 0
-    num_attempts = 0
     accept_trajectory_key = target
     noise = 0.1
     EPSILON = 0.1
 
     while num_saved < num_trajectories:
-        num_attempts += 1
+        num_saved += 1
         num_steps = 1e6
         rewards = []
         env.reset()
@@ -56,15 +55,13 @@ def collect_data(env, model, policy, target, num_trajectories=100, num_timesteps
             observation["image"] = np.transpose(observation["image"], (2, 0, 1))
             next_observation, reward, done, info = env.step(action)
             next_observation["image"] = np.transpose(next_observation["image"], (2, 0, 1))
-            if num_steps <1e3:
-                reward = 0
             rewards.append(reward)
             model.replay_buffer.add(observation, next_observation, action, reward, np.array([done]), [{}])
 
             if info[accept_trajectory_key] and num_steps > 1e3:
                 num_steps = j
 
-            if info[accept_trajectory_key] and j > 16:
+            if info[accept_trajectory_key] and j > 23:
                 break
             if done or agent_info['done']:
                 break
@@ -73,14 +70,13 @@ def collect_data(env, model, policy, target, num_trajectories=100, num_timesteps
             PRINT = False
             if PRINT:
                 print("num_timesteps: ", num_steps, rewards)
-                print(observation["image"].shape)
-                print(next_observation["image"].shape)
+                #print(observation["image"].shape)
+                #print(next_observation["image"].shape)
             num_success += 1
-            num_saved += 1
         if num_saved%100 == 0:
-            print(f"num_trajectories: {num_saved} success rate: {num_success/num_attempts} Reward: {sum(rewards)}")
+            print(f"num_trajectories: {num_saved} success rate: {num_success/num_saved} Reward: {sum(rewards)}")
 
-    print("success rate: {}".format(num_success / (num_attempts)))
+    print("success rate: {}".format(num_success / (num_saved)))
 
 
 import gymnasium as gym
@@ -97,7 +93,7 @@ from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
 from stable_baselines3.common.callbacks import EvalCallback, CheckpointCallback
 
 
-env = roboverse.make("Widow250PickPlace-v1",
+env = roboverse.make("Widow250PickPlace-v2",
                          gui=False,
                          observation_mode="pixels",
                          transpose_image=False)
@@ -114,27 +110,34 @@ checkpoint_callback = CheckpointCallback(
   save_vecnormalize=False,
 )
 
-model = TQC(env=env, batch_size=2048, buffer_size=200000, gamma=0.95, learning_rate=0.001, policy='MultiInputPolicy',
+model = TQC(env=env, batch_size=2048, buffer_size=1_000_000, gamma=0.95, learning_rate=0.001, policy='MultiInputPolicy',
              policy_kwargs=dict(net_arch=[512, 512, 512], n_critics=2),
-             replay_buffer_class=HerReplayBuffer,
-             replay_buffer_kwargs=dict(goal_selection_strategy='future', n_sampled_goal=4),
+             #replay_buffer_class=HerReplayBuffer,
+             #replay_buffer_kwargs=dict(goal_selection_strategy='future', n_sampled_goal=4),
              tau=0.05, learning_starts=0, verbose=1)
 
+#model = TQC.load("data/tqc")
+#model.set_env(env)
 COLLECT=True
 if COLLECT:
-    collect_data(env, model, "grasp", "grasp_success_target", 5000, 30)
-    model.save_replay_buffer(f"data/tqc_expert_grasp")
+    collect_data(env, model, "pickplace", "place_success_target", 10000, 35)
+    model.save_replay_buffer(f"data/tqc_expert_pick_place")
 else:
     print("load_replay_buffer")
-    model.load_replay_buffer(f"data/tqc_expert_grasp")
+    model.load_replay_buffer(f"data/tqc_expert_pick_place")
 
 # print("start pre-training from buffer only")
-# model.learn(total_timesteps=0, log_interval=5, tb_log_name="exp", reset_num_timesteps = False, progress_bar=True)
-# model.train(gradient_steps=10000)
+# model.learn(total_timesteps=0, callback=checkpoint_callback, log_interval=5, tb_log_name="exp", reset_num_timesteps = False, progress_bar=True)
+# model.train(gradient_steps=20000)
 
 print("start learning")
-model.learn(total_timesteps=20000, callback=checkpoint_callback, log_interval=5, tb_log_name="exp", reset_num_timesteps = False, progress_bar=True)
-model.save("data/tqc")
-model.save_replay_buffer(f"data/tqc_expert_grasp{i+1}")
+model.learn(total_timesteps=500_000, callback=checkpoint_callback, log_interval=5, tb_log_name="exp", reset_num_timesteps = False, progress_bar=True)
+model.save("data/tqc_pick_place")
+model.save_replay_buffer(f"data/tqc_expert_pick_place_trained")
+
+collect_data(env, model, "pickplace", "place_success_target", 10000, 35)
+model.learn(total_timesteps=500_000, callback=checkpoint_callback, log_interval=5, tb_log_name="exp", reset_num_timesteps = False, progress_bar=True)
+model.save("data/tqc_pick_place")
+model.save_replay_buffer(f"data/tqc_expert_pick_place_trained")
 
 print("finish learning")
